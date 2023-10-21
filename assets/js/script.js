af2e1252198e631e81e9c60e4f3ed467674e83bf
KenPlearnscoding <126117889+KenLearnsCoding@users.noreply.github.com>
2023-10-20 21:48:48 -0400

    push

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,8 +1,11 @@
+// all html and css will use this one js file to use responsive bar 
+
+// the event is clicked then change the css property
 function showSideBar () {
     const sidebar = document.querySelector('.sideBar');
     sidebar.style.display = 'flex'
 }
-
+// the event is clicked then change the css property
 function hideSideBar() {
     const sidebar = document.querySelector('.sideBar');
     sidebar.style.display = 'none'
